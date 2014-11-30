50e611ff4fe6432456de58a76fa4749fbbc98449
MarcoAurelio <A.F.Pala@warwick.ac.uk>
2014-11-30 21:21:31 +0000

    Update Exercise 4

--- PP6Lib/PP6Day4.cpp
+++ PP6Lib/PP6Day4.cpp
@@ -1,6 +1,7 @@
 #include "PP6Day4.hpp"
 #include "PP6FourVector.hpp"
 #include "PP6ParticleInfo.hpp"
+#include <PP6Particle.hpp>
 #include "FileReaderTemplate.hpp"
 
 #include <iostream>
@@ -13,6 +14,10 @@
 #include "PP6Math.hpp"
 
 void do_day4(){
+  std::vector<std::string> particle_name;
+  std::vector<int> pdg_code;
+  std::vector<int> charge;
+  std::vector<double> mass;
   std::vector<int> event;
   std::vector<std::string> name;
   std::vector<std::string> run;
@@ -20,6 +25,30 @@ void do_day4(){
   std::vector<double> py; 
   std::vector<double> pz;
   std::string path;
+  std::string database;
+
+  //Create database
+  std::cout << "Enter database path: ";
+  std::cin >> database;
+  FileReaderTemplate f(database);
+  std::vector<std::string> line_database;
+
+  if (f.isValid()) {
+     // Loop until out of lines
+     while (file.nextLine()) {
+     // Fields in each line line are treated as whitespace separated
+     // and counted from 1. 
+
+     for ( int i : line_database){
+         particle_name.push_back(f.getField(1, particle_name));
+         pdg_code.push_back(f.getField(2, pdg_code);
+         charge.push_back(f.getField(3, charge));
+         mass.push_back(f.getField(4, mass));
+     }
+     }
+  }
+
+//  mu_mass = 
 
   // Obtain filename from user
   std::cout << "Enter filename to analyse: ";
